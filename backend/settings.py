1820bdfd339dc827be2d17cff0f7dfdb9487c8e2
win21g <win21g@mail.ru>
2020-08-02 16:31:46 +0300

    change smtp

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$2s81)%!49cba64kgl0hbrwk3y0yqfch$a408h##)6u_$6mrij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.percale.com.ua']
 #ALLOWED_HOSTS = []
