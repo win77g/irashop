d211f4af5250da26b7f8a57162384f84b751ffe6
win21g <win21g@mail.ru>
2020-08-05 15:34:58 +0300

    new setting

--- backend/settings.py
+++ backend/settings.py
@@ -237,11 +237,11 @@ DJOSER = {
 #         # 'current_user':'authentication.serializers.CurrentUserSerializer',
 #     }
 # }
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-# EMAIL_HOST = SendMail.host
-# EMAIL_HOST_USER = SendMail.email
-# EMAIL_HOST_PASSWORD = SendMail.password
-# EMAIL_PORT =  587
-# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+# SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+EMAIL_PORT =  587
+EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
