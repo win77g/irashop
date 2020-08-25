c9471ebc2c0d4f9f7ee11e785a927478a0f21a04
win21g <win21g@mail.ru>
2020-08-25 11:03:03 +0300

    new main 25.08

--- backend/settings.py
+++ backend/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$2s81)%!49cba64kgl0hbrwk3y0yqfch$a408h##)6u_$6mrij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+# DEBUG = True
 DEBUG = False
 
 
 ALLOWED_HOSTS = ['www.percale.com.ua']
-#ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 # SENDGRID_SANDBOX_MODE_IN_DEBUG=False
 # if DEBUG:
 #     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
